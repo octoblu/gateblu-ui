242b6146c85dbd4619b94571f6eeb06ca583641b
Jade Meskill <jade.meskill@gmail.com>
2014-10-15 17:47:02 -0700

    make sure you have a gateblu.dmg, bless thine holy opening

--- scripts/build-osx.sh
+++ scripts/build-osx.sh
@@ -2,6 +2,11 @@
 
 OWD=`pwd`
 
+if [ ! -f Gateblu.dmg ]; then
+  echo "You need a Gateblu.dmg in your current dir"
+  exit 1
+fi
+
 hdiutil attach Gateblu.dmg
 
 cd /Volumes/Gateblu/Gateblu.app/Contents/Resources/app.nw
@@ -10,6 +15,7 @@ rm -rf node_modules
 cp -r $OWD/node_modules .
 
 cd $OWD
+bless --folder /Volumes/Gateblu --openfolder /Volumes/Gateblu
 sleep 2
 
 hdiutil detach /Volumes/Gateblu
