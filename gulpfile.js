ce92ae18e2e0695c7055bde650c3b10c9c99ba0b
redaphid <iam@aaronherres.com>
2014-10-23 11:46:05 -0700

    simpler folder structure for node-webkit

--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,16 @@ var concat = require('gulp-concat');
 var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('coffee', function(){
-  gulp.src('./public/source/**/*.coffee')
+  gulp.src('./src/**/*.coffee')
     .pipe(sourcemaps.init())
     .pipe(coffee())
-    .pipe((concat('application.js')))
+    .pipe((concat('dist.js')))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./public/dist'))
+    .pipe(gulp.dest('./src'))
 });
 
 gulp.task('default', ['coffee']);
 
 gulp.task('watch', ['default'], function() {
-  gulp.watch(['./public/source/**/*.coffee'], ['coffee']);
+  gulp.watch(['./src/**/*.coffee'], ['coffee']);
 });
--- public/source/app.coffee
+++
@@ -1,1 +0,0 @@
-angular.module 'gateblu-ui', []
