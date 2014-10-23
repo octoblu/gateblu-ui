92c89f2d8c8d8b32a997e98ec22a927ebdadd225
Jade Meskill <jade.meskill@gmail.com>
2014-10-23 13:33:24 -0700

    log uncaught exceptions and device changes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
-(function(){
+$(function(){
 'use strict';
 
 var fs      = require('fs-extra');
 var rimraf  = require('rimraf');
 var request = require('request');
+var _       = require('lodash');
 
 var $             = require('jquery');
 var version       = require('./package.json').version;
 var configManager = require('./src/config-manager');
 var config        = configManager.loadConfig();
+
+process.on('uncaughtException', function(error){
+  console.error(error.message);
+  console.error(error.stack);
+  $('ul.logs').append('<li>' + error.message + '</li>');
+});
 
 if (!config) {
   configManager.saveConfig();
@@ -22,21 +29,40 @@ var Gateblu = require('gateblu');
 var gateblu = new Gateblu(config);
 
 gateblu.on('config', function(config){
-  $('ul.log').append('<li>Connected to Meshblu. UUID: '+config.uuid+'</li>');
-  $('ul.log').append('<li>Goto <a href="https://app.octoblu.com/connect/nodes/" class="external-link">Octoblu</a> to configure the Gateblu</li>');
+  $('ul.statuses').append('<li>Connected to Meshblu. UUID: '+config.uuid+'</li>');
+  $('ul.statuses').append('<li>Goto <a href="https://app.octoblu.com/connect/nodes/" class="external-link">Octoblu</a> to configure the Gateblu</li>');
   configManager.saveConfig(config);
 });
 
+gateblu.on('update', function(devices){
+  _.each(devices, function(device){
+    var html = '<li data-uuid="'+device.uuid+'">' + device.type + '(' + device.uuid + ')</li>';
+    $('ul.devices').append(html);
+  })
+});
+
 gateblu.on('device:start', function(device){
-  $('ul.devices').append('<li>' + device.type + '(' + device.uuid + ')</li>' );
+  $('ul.devices li[data-uuid=' + device.uuid + ']').addClass('active');
 });
 
 gateblu.on('refresh', function(){
   $('ul.devices').html('');
+  $('.logs').append('Refreshing Device List' + '\n');
+});
+
+gateblu.on('stderr', function(data, device){
+  $('.logs').append("Error: " + device.name + '\n');
+  $('.logs').append(data);
+});
+
+gateblu.on('stdout', function(data, device){
+  $('.logs').append((device.name || device.uuid) + '\n');
+  $('.logs').append(data);
 });
 
 request.get('http://gateblu.octoblu.com/version.json', {json: true}, function(error, response, body){
   if(error){
+    $('.logs').append(error.message + '\n');
     return;
   }
 
@@ -45,4 +71,4 @@ request.get('http://gateblu.octoblu.com/version.json', {json: true}, function(er
   }
 });
 
-})();
+});
