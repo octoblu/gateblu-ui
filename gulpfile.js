9e409568895c6a786bbf9e851f4db7853c79c2ec
Jade Meskill <jade.meskill@gmail.com>
2014-10-23 13:22:21 -0700

    compile coffee into dist

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,12 @@
 var gulp         = require('gulp');
 var coffee = require('gulp-coffee');
 var concat = require('gulp-concat');
-var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('coffee', function(){
   gulp.src('./src/**/*.coffee')
-    .pipe(sourcemaps.init())
     .pipe(coffee())
-    .pipe((concat('dist.js')))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./src'))
+    .pipe((concat('application.js')))
+    .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('default', ['coffee']);
